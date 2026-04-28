e77d84cba3e373a85efd5ebf1f0d3d70c8c8032e
Katya <guelder.rose8@gmail.com>
2026-04-28 14:18:40 +0000

    Clang formatting is added

--- include/libmath.h
+++ include/libmath.h
@@ -1,10 +1,9 @@
 #pragma once
 
-namespace libmath
-{
-    int addition(int a, int b);
-    int subtraction(int a, int b);
-    int multiplication(int a, int b);
-    int division(int a, int b, int& c);
-    int power(int a, int b);
-}
+namespace libmath {
+int addition(int a, int b);
+int subtraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b, int &c);
+int power(int a, int b);
+} // namespace libmath
